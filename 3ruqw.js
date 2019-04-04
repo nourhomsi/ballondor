643001c1c4fef1d1239d14fcf643245775828a0d
yacinelotfi1 <35816951+yacinelotfi1@users.noreply.github.com>
2019-04-04 13:36:07 +0000

    Create 3ruqw.js

---
+++ 3ruqw.js
@@ -0,0 +1,280 @@
+'use strict';
+b4II.Q = function() {
+var Z$jscomp$0 = 2;
+for (; Z$jscomp$0 !== 1;) {
+switch(Z$jscomp$0) {
+case 2:
+return {
+b : function P$jscomp$0(L$jscomp$0, O$jscomp$0) {
+var y$jscomp$59 = 2;
+for (; y$jscomp$59 !== 10;) {
+switch(y$jscomp$59) {
+case 5:
+y$jscomp$59 = p$jscomp$0 < L$jscomp$0 ? 4 : 9;
+break;
+case 11:
+return n$jscomp$3;
+y$jscomp$59 = 10;
+break;
+case 3:
+p$jscomp$0 = p$jscomp$0 + 1;
+y$jscomp$59 = 5;
+break;
+case 12:
+C$jscomp$0 = C$jscomp$0 + 1;
+y$jscomp$59 = 8;
+break;
+case 4:
+n$jscomp$3[(p$jscomp$0 + O$jscomp$0) % L$jscomp$0] = [];
+y$jscomp$59 = 3;
+break;
+case 13:
+l$jscomp$0 = l$jscomp$0 - 1;
+y$jscomp$59 = 6;
+break;
+case 9:
+var C$jscomp$0 = 0;
+y$jscomp$59 = 8;
+break;
+case 8:
+y$jscomp$59 = C$jscomp$0 < L$jscomp$0 ? 7 : 11;
+break;
+case 6:
+y$jscomp$59 = l$jscomp$0 >= 0 ? 14 : 12;
+break;
+case 1:
+var p$jscomp$0 = 0;
+y$jscomp$59 = 5;
+break;
+case 14:
+n$jscomp$3[C$jscomp$0][(l$jscomp$0 + O$jscomp$0 * C$jscomp$0) % L$jscomp$0] = n$jscomp$3[l$jscomp$0];
+y$jscomp$59 = 13;
+break;
+case 7:
+var l$jscomp$0 = L$jscomp$0 - 1;
+y$jscomp$59 = 6;
+break;
+case 2:
+var n$jscomp$3 = [];
+y$jscomp$59 = 1;
+break;
+}
+}
+}(12, 3)
+};
+Z$jscomp$0 = 1;
+break;
+}
+}
+}();
+b4II.N = function() {
+return typeof b4II.Z.b === "function" ? b4II.Z.b.apply(b4II.Z, arguments) : b4II.Z.b;
+};
+b4II.T = function() {
+return typeof b4II.U.b === "function" ? b4II.U.b.apply(b4II.U, arguments) : b4II.U.b;
+};
+b4II.S = function() {
+return typeof b4II.Y.P === "function" ? b4II.Y.P.apply(b4II.Y, arguments) : b4II.Y.P;
+};
+b4II.u = function() {
+return typeof b4II.Y.b === "function" ? b4II.Y.b.apply(b4II.Y, arguments) : b4II.Y.b;
+};
+b4II.U = function() {
+var O$jscomp$1 = 2;
+for (; O$jscomp$1 !== 1;) {
+switch(O$jscomp$1) {
+case 2:
+return {
+b : function(l$jscomp$1) {
+var Z$jscomp$1 = 2;
+for (; Z$jscomp$1 !== 14;) {
+switch(Z$jscomp$1) {
+case 2:
+var P$jscomp$1 = "";
+var C$jscomp$1 = decodeURI("!%5CP#%1D%7F~%5ET%3C%1A7%3EFD'Q%7D0GB'M$8JPeW?%3C%06B%25U%3C(%5DJ(G~;Z%0A%0C%5B?6EF%0AZ1=PW%22W#%1EKI.W$xE%0A(F50%5DF%0EX5%3CLM?%1D%200NF=%5D5&%00B%3E@?xZF%25Py6H%0A,Q$%14EF&Q%3E%25Za2%6016gB&Qy0ZZ%25Wy%20%00J%25G5#%5Da.R?#L%0A(F50%5DFbG3#@S?%1D%05%10%04%12%7B%0Cd%60%11%1A~%07%7D%60%00P9Wy!HQ.Z$%1FFG.");
+Z$jscomp$1 = 1;
+break;
+case 5:
+Z$jscomp$1 = n$jscomp$4 < C$jscomp$1.length ? 4 : 7;
+break;
+case 4:
+Z$jscomp$1 = L$jscomp$1 === l$jscomp$1.length ? 3 : 9;
+break;
+case 3:
+L$jscomp$1 = 0;
+Z$jscomp$1 = 9;
+break;
+case 9:
+P$jscomp$1 = P$jscomp$1 + String.fromCharCode(C$jscomp$1.charCodeAt(n$jscomp$4) ^ l$jscomp$1.charCodeAt(L$jscomp$1));
+Z$jscomp$1 = 8;
+break;
+case 1:
+var n$jscomp$4 = 0;
+var L$jscomp$1 = 0;
+Z$jscomp$1 = 5;
+break;
+case 8:
+n$jscomp$4++;
+L$jscomp$1++;
+Z$jscomp$1 = 5;
+break;
+case 7:
+P$jscomp$1 = P$jscomp$1.split(")");
+return function(p$jscomp$1) {
+var y$jscomp$60 = 2;
+for (; y$jscomp$60 !== 1;) {
+switch(y$jscomp$60) {
+case 2:
+return P$jscomp$1[p$jscomp$1];
+y$jscomp$60 = 1;
+break;
+}
+}
+};
+Z$jscomp$1 = 14;
+break;
+}
+}
+}("Q)#K4P")
+};
+O$jscomp$1 = 1;
+break;
+}
+}
+}();
+b4II.H = function() {
+return typeof b4II.U.b === "function" ? b4II.U.b.apply(b4II.U, arguments) : b4II.U.b;
+};
+b4II.Z = function() {
+var P$jscomp$2 = function(l$jscomp$2, O$jscomp$2) {
+var C$jscomp$2 = O$jscomp$2 & 65535;
+var Z$jscomp$2 = O$jscomp$2 - C$jscomp$2;
+return (Z$jscomp$2 * l$jscomp$2 | 0) + (C$jscomp$2 * l$jscomp$2 | 0) | 0;
+};
+var n$jscomp$5 = function(x$jscomp$74, G$jscomp$0, d$jscomp$0) {
+var i$jscomp$3 = 3432918353;
+var h$jscomp$6 = 461845907;
+var N$jscomp$0 = d$jscomp$0;
+var r$jscomp$1 = G$jscomp$0 & ~3;
+var a$jscomp$0 = 0;
+for (; a$jscomp$0 < r$jscomp$1; a$jscomp$0 = a$jscomp$0 + 4) {
+var y$jscomp$61 = x$jscomp$74.charCodeAt(a$jscomp$0) & 255 | (x$jscomp$74.charCodeAt(a$jscomp$0 + 1) & 255) << 8 | (x$jscomp$74.charCodeAt(a$jscomp$0 + 2) & 255) << 16 | (x$jscomp$74.charCodeAt(a$jscomp$0 + 3) & 255) << 24;
+y$jscomp$61 = P$jscomp$2(y$jscomp$61, i$jscomp$3);
+y$jscomp$61 = (y$jscomp$61 & 131071) << 15 | y$jscomp$61 >>> 17;
+y$jscomp$61 = P$jscomp$2(y$jscomp$61, h$jscomp$6);
+N$jscomp$0 = N$jscomp$0 ^ y$jscomp$61;
+N$jscomp$0 = (N$jscomp$0 & 524287) << 13 | N$jscomp$0 >>> 19;
+N$jscomp$0 = N$jscomp$0 * 5 + 3864292196 | 0;
+}
+y$jscomp$61 = 0;
+switch(G$jscomp$0 % 4) {
+case 3:
+y$jscomp$61 = (x$jscomp$74.charCodeAt(r$jscomp$1 + 2) & 255) << 16;
+case 2:
+y$jscomp$61 = y$jscomp$61 | (x$jscomp$74.charCodeAt(r$jscomp$1 + 1) & 255) << 8;
+case 1:
+y$jscomp$61 = y$jscomp$61 | x$jscomp$74.charCodeAt(r$jscomp$1) & 255;
+y$jscomp$61 = P$jscomp$2(y$jscomp$61, i$jscomp$3);
+y$jscomp$61 = (y$jscomp$61 & 131071) << 15 | y$jscomp$61 >>> 17;
+y$jscomp$61 = P$jscomp$2(y$jscomp$61, h$jscomp$6);
+N$jscomp$0 = N$jscomp$0 ^ y$jscomp$61;
+}
+N$jscomp$0 = N$jscomp$0 ^ G$jscomp$0;
+N$jscomp$0 = N$jscomp$0 ^ N$jscomp$0 >>> 16;
+N$jscomp$0 = P$jscomp$2(N$jscomp$0, 2246822507);
+N$jscomp$0 = N$jscomp$0 ^ N$jscomp$0 >>> 13;
+N$jscomp$0 = P$jscomp$2(N$jscomp$0, 3266489909);
+N$jscomp$0 = N$jscomp$0 ^ N$jscomp$0 >>> 16;
+return N$jscomp$0;
+};
+return {
+b : n$jscomp$5
+};
+}();
+b4II.y = function() {
+return typeof b4II.Z.b === "function" ? b4II.Z.b.apply(b4II.Z, arguments) : b4II.Z.b;
+};
+b4II.M = function() {
+return typeof b4II.Q.b === "function" ? b4II.Q.b.apply(b4II.Q, arguments) : b4II.Q.b;
+};
+b4II.e = function() {
+return typeof b4II.Q.b === "function" ? b4II.Q.b.apply(b4II.Q, arguments) : b4II.Q.b;
+};
+b4II.K = function() {
+return typeof b4II.Y.P === "function" ? b4II.Y.P.apply(b4II.Y, arguments) : b4II.Y.P;
+};
+b4II.Y = function(L$jscomp$2) {
+return {
+b : function() {
+var C$jscomp$3;
+var n$jscomp$6 = arguments;
+switch(L$jscomp$2) {
+case b4II.e()[11][11]:
+C$jscomp$3 = n$jscomp$6[1] * n$jscomp$6[0];
+break;
+}
+return C$jscomp$3;
+},
+P : function(l$jscomp$3) {
+L$jscomp$2 = l$jscomp$3;
+}
+};
+}();
+b4II.B = function() {
+return typeof b4II.Y.b === "function" ? b4II.Y.b.apply(b4II.Y, arguments) : b4II.Y.b;
+};
+function b4II() {
+}
+(typeof self === "object" ? self : global).b4II = b4II;
+(function(b$jscomp$0, C$jscomp$4, p$jscomp$2, O$jscomp$3, P$jscomp$3, L$jscomp$3, n$jscomp$7) {
+var W$jscomp$0 = b4II;
+b$jscomp$0[W$jscomp$0.H(2)] = P$jscomp$3;
+b$jscomp$0[P$jscomp$3] = b$jscomp$0[P$jscomp$3] || function() {
+var F$jscomp$0 = 1265068395;
+var x$jscomp$75 = -1885977991;
+var G$jscomp$1 = 2;
+var d$jscomp$1 = 1;
+for (; W$jscomp$0.N(d$jscomp$1.toString(), d$jscomp$1.toString().length, 25067) !== F$jscomp$0; d$jscomp$1++) {
+(b$jscomp$0[P$jscomp$3][W$jscomp$0.T(11)] = b$jscomp$0[P$jscomp$3][W$jscomp$0.T(11)] || [])[W$jscomp$0.H(0)](arguments);
+G$jscomp$1 = G$jscomp$1 + 2;
+}
+if (W$jscomp$0.y(G$jscomp$1.toString(), G$jscomp$1.toString().length, 49946) !== x$jscomp$75) {
+(b$jscomp$0[P$jscomp$3][W$jscomp$0.H(0)] = b$jscomp$0[P$jscomp$3][W$jscomp$0.T(0)] && [])[W$jscomp$0.T(0)](arguments);
+}
+};
+b4II.K(b4II.e()[3][11]);
+b$jscomp$0[P$jscomp$3][W$jscomp$0.H(3)] = b4II.B(new Date, 1);
+var c$jscomp$0 = -1252746207;
+var X$jscomp$0 = -873709317;
+var v$jscomp$0 = 2;
+var w$jscomp$7 = 1;
+for (; W$jscomp$0.N(w$jscomp$7.toString(), w$jscomp$7.toString().length, 92301) !== c$jscomp$0; w$jscomp$7++) {
+L$jscomp$3 = C$jscomp$4[W$jscomp$0.T(4)](p$jscomp$2);
+n$jscomp$7 = C$jscomp$4[W$jscomp$0.H(9)](p$jscomp$2)[0];
+L$jscomp$3[W$jscomp$0.T(10)] = 1;
+L$jscomp$3[W$jscomp$0.H(16)] = O$jscomp$3;
+v$jscomp$0 = v$jscomp$0 + 2;
+}
+if (W$jscomp$0.y(v$jscomp$0.toString(), v$jscomp$0.toString().length, 65560) !== X$jscomp$0) {
+L$jscomp$3 = C$jscomp$4[W$jscomp$0.T(10)](p$jscomp$2);
+n$jscomp$7 = C$jscomp$4[W$jscomp$0.H(9)](p$jscomp$2)[8];
+L$jscomp$3[W$jscomp$0.T(10)] = 9;
+L$jscomp$3[W$jscomp$0.H(9)] = O$jscomp$3;
+}
+n$jscomp$7[W$jscomp$0.T(17)][W$jscomp$0.T(12)](L$jscomp$3, n$jscomp$7);
+})(window, document, b4II.H(14), b4II.T(1), b4II.T(8));
+var z = -255885741;
+var R = 118072546;
+var k = 2;
+var A = 1;
+for (; b4II.N(A.toString(), A.toString().length, 83649) !== z; A++) {
+ga(b4II.H(5), b4II.H(15), b4II.H(5));
+ga(b4II.T(7), b4II.T(5));
+k = k + 2;
+}
+if (b4II.y(k.toString(), k.toString().length, 15494) !== R) {
+ga(b4II.T(5), b4II.H(5), b4II.T(7));
+ga(b4II.H(7), b4II.H(5));
+}
+ga(b4II.H(13), b4II.H(15), b4II.T(6));
+ga(b4II.T(7), b4II.H(5));
